377d3484ad8253bf8bb401184f3a0f5ce92d741e
amit35352 <64771179+amit35352@users.noreply.github.com>
2020-06-13 00:33:36 +0530

    Update existing.bat

--- existing.bat
+++ existing.bat
@@ -1,5 +1,5 @@
 call config.bat
-usr=1
+set usr=1
 if usr==1 (cd C:\Users\DEII\.jenkins\workspace\Demo_Proj\Robot_Xray\batches)
 if usr==2 (cd C:\Users\DEII\.jenkins\workspace\Demo_Proj\Robot_Xray)
 for /f "delims=" %%a in ('dir /s /b output*.xml') do set "name=%%a"
