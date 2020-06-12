f31f5eb17ab37ad933bdfc78179d8958b96dd6b9
amit35352 <64771179+amit35352@users.noreply.github.com>
2020-06-12 23:45:23 +0530

    Update existing.bat

--- existing.bat
+++ existing.bat
@@ -1,3 +1,4 @@
 call config.bat
-for /f "delims=" %%a in ('C:\Users\DEII\.jenkins\workspace\Demo_Proj\Robot_Xray /s /b output*.xml') do set "name=%%a"
+cd C:\Users\DEII\.jenkins\workspace\Demo_Proj\Robot_Xray
+for /f "delims=" %%a in ('dir /s /b output*.xml') do set "name=%%a"
 curl -H "Content-Type: multipart/form-data" -u "sp.testing2018":"sptesting2" -F "file=@%name%" "%rootURL%/rest/raven/1.0/import/execution/robot/?testExecKey=%teIssue%"
